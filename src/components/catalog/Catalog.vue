04b15c0303db9ca5e4651c39685e974741a8bcf3
unknown <rifatkhabibulin88@gmail.comgit config --global user.name Rifatgit statusgit config user.email rifatkhabibulin88@gmail.com>
2023-02-05 13:19:22 +0300

    feat: add price filter

--- src/components/catalog/Catalog.vue
+++ src/components/catalog/Catalog.vue
@@ -6,11 +6,35 @@
           </div>
         </router-link>
         <h1>Catalog</h1>
-        <v-select
+        <div class="filters">
+          <v-select
           :options="categories"
           :selected="selected"
           @select="sortByCategories"
         />
+          <div class="range-slider">
+            <input 
+            type="range"
+            min="0"
+            max="10000"
+            step="50"
+            v-model.number="minPrice"
+            @change="setRangeSlider"
+            >
+            <input 
+            type="range"
+            min="0"
+            max="10000"
+            step="50"
+            v-model.number="maxPrice"
+            @change="setRangeSlider"
+            >
+          </div>
+          <div class="range-values">
+            <p>Min: {{ minPrice }}</p>
+            <p>Max: {{ maxPrice }}</p>
+          </div>
+        </div>
         <div class="catalog__list">
           <catalog-item 
             v-for="product in filteredProducts"
@@ -39,7 +63,9 @@
               {name: 'Женские', value: 'ж'},
             ],
             selected: 'Все',
-            sortedProducts: []
+            sortedProducts: [],
+            minPrice: 0,
+            maxPrice: 10000
         }
     },
     computed: {
@@ -60,15 +86,34 @@
       addToCart(data) {
         this.ADD_TO_CART(data);
       },
+      setRangeSlider() {
+        if (this.minPrice > this.maxPrice) {
+          let tmp = this.maxPrice;
+          this.maxPrice = this.minPrice;
+          this.minPrice = tmp;
+        }
+        this.sortByCategories();
+      },
       sortByCategories(category) {
-        this.sortedProducts = [];
+        // this.sortedProducts = [];
+        // let vm = this;
+        // vm.PRODUCTS.map((item) => {
+        //   if (item.category === category.name) {
+        //     this.sortedProducts.push(item);
+        //   }
+        // });
+        // this.selected = category.name;
         let vm = this;
-        vm.PRODUCTS.map((item) => {
-          if (item.category === category.name) {
-            this.sortedProducts.push(item);
-          }
-        });
-        this.selected = category.name;
+        this.sortedProducts = [...this.PRODUCTS];
+        this.sortedProducts = this.sortedProducts.filter(function(item) {
+          return item.price >= vm.minPrice && item.price <= vm.maxPrice;
+        })
+        if (category) {
+          this.sortedProducts = this.sortedProducts.filter(function(el) {
+            vm.selected = category.name;
+            return el.category === category.name;
+        })
+        }
       }
     },
     mounted() {
@@ -76,6 +121,7 @@
         .then((response) => {
           if (response.data) {
             console.log('Data received!');
+            this.setRangeSlider();
           }
         })
     }
@@ -98,5 +144,31 @@
         border: solid 1px #aeaeaa;
         border-radius: $radius;
       }
+    }
+
+    .filters {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .range-slider {
+      width: 200px;
+      margin: auto 16px;
+      text-align: center;
+      position: relative;
+    }
+
+    .range-slider svg, .range-slider input[type="range"] {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+    }
+
+    input[type="range"]::-webkit-slider-thumb {
+      z-index: 2;
+      position: relative;
+      top: 2px;
+      margin-top: -7px;
     }
 </style>
