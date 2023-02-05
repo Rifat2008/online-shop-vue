e875cfdab4b754fb61acb4bf82a7947eebe0bb62
unknown <rifatkhabibulin88@gmail.comgit config --global user.name Rifatgit statusgit config user.email rifatkhabibulin88@gmail.com>
2023-02-05 20:23:09 +0300

    feat: add popup

--- src/components/catalog/CatalogItem.vue
+++ src/components/catalog/CatalogItem.vue
@@ -1,20 +1,44 @@
 <template>
     <div class="catalog-item">
+
+
+      <popup
+        v-if="isInfoPopupVisible"
+        submitBtnTitle="Add to cart"
+        :popupTitle="productData.name"
+        @closePopup="closeInfoPopup"
+        @action="add"
+      >
         <img class="catalog-item__image" :src=" require('../../assets/images/' + productData.image) " alt="img">
-        <p class="catalog-item__name">{{ productData.name }}</p>
-        <p class="catalog-item__price">Price: {{ productData.price }} Р.</p>
-        <button 
-          class="catalog-item__add_to_cart_btn btn" 
-          @click="add"
+        <div class="">
+          <p class="catalog-item__price">Price: {{ productData.price }} Р.</p>
+          <p class="catalog-item__price">Category: {{ productData.category }}</p>
+        </div>
+      </popup>
+
+        
+      <img class="catalog-item__image" :src=" require('../../assets/images/' + productData.image) " alt="img">
+      <p class="catalog-item__name">{{ productData.name }}</p>
+      <p class="catalog-item__price">Price: {{ productData.price }} Р.</p>
+      <button
+        class="catalog-item__show-info"
+        @click="showPopupInfo"
+        >Show info
+      </button>
+      <button 
+        class="catalog-item__add_to_cart_btn btn" 
+        @click="add"
         >Add to cart
       </button>
     </div>    
 </template>
 
 <script>
+  import Popup from '../popup/Popup.vue';
+
   export default {
     name: 'catalog-item',
-    components: {},
+    components: {Popup},
     emits:['sendArticle', 'addToCart'],
     props: {
       productData: {
@@ -24,7 +48,18 @@
         }
       }
     },
+    data() {
+      return {
+        isInfoPopupVisible: false
+      }
+    },
     methods: {
+      showPopupInfo() {
+        this.isInfoPopupVisible = true;
+      },
+      closeInfoPopup() {
+        this.isInfoPopupVisible = false;
+      },
       add() {
         this.$emit('addToCart',this.productData)
       }
